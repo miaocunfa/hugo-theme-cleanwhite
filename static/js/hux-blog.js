0d34a7a26e7cb06efc1c4c0392925701376f3023
miao <miaocunf@163.com>
2021-01-29 18:04:48 +0800

    定制化修改

--- static/js/hux-blog.js
+++ static/js/hux-blog.js
@@ -35,18 +35,52 @@ $(document).ready(function () {
 });
 
 // Navigation Scripts to Show Header on Scroll-Up
+// jQuery(document).ready(function($) {
+//     var MQL = 1170;
+
+//     //primary navigation slide-in effect
+//     if ($(window).width() > MQL) {
+//         var headerHeight = $('.navbar-custom').height();
+//         $(window).on('scroll', {
+//                 previousTop: 0
+//             },
+//             function() {
+//                 var currentTop = $(window).scrollTop();
+//                 //check if user is scrolling up
+//                 if (currentTop < this.previousTop) {
+//                     //if scrolling up...
+//                     if (currentTop > 0 && $('.navbar-custom').hasClass('is-fixed')) {
+//                         $('.navbar-custom').addClass('is-visible');
+//                     } else {
+//                         $('.navbar-custom').removeClass('is-visible is-fixed');
+//                     }
+//                 } else {
+//                     //if scrolling down...
+//                     $('.navbar-custom').removeClass('is-visible');
+//                     if (currentTop > headerHeight && !$('.navbar-custom').hasClass('is-fixed')) $('.navbar-custom').addClass('is-fixed');
+//                 }
+//                 this.previousTop = currentTop;
+//             });
+//     }
+// });
+
+
+// Navigation Scripts to Show Header on Scroll-Up
 jQuery(document).ready(function($) {
     var MQL = 1170;
 
     //primary navigation slide-in effect
     if ($(window).width() > MQL) {
-        var headerHeight = $('.navbar-custom').height();
+        var headerHeight = $('.navbar-custom').height(),
+            bannerHeight  = $('.intro-header .container').height();     
         $(window).on('scroll', {
                 previousTop: 0
             },
             function() {
-                var currentTop = $(window).scrollTop();
-                //check if user is scrolling up
+                var currentTop = $(window).scrollTop(),
+                    $catalog = $('.side-catalog');
+
+                //check if user is scrolling up by mouse or keyborad
                 if (currentTop < this.previousTop) {
                     //if scrolling up...
                     if (currentTop > 0 && $('.navbar-custom').hasClass('is-fixed')) {
@@ -60,6 +94,15 @@ jQuery(document).ready(function($) {
                     if (currentTop > headerHeight && !$('.navbar-custom').hasClass('is-fixed')) $('.navbar-custom').addClass('is-fixed');
                 }
                 this.previousTop = currentTop;
+
+
+                //adjust the appearance of side-catalog
+                $catalog.show()
+                if (currentTop > (bannerHeight + 41)) {
+                    $catalog.addClass('fixed')
+                } else {
+                    $catalog.removeClass('fixed')
+                }
             });
     }
 });
